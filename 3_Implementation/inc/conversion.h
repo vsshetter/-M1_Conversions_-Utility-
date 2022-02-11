b8aee89580ee2e934f2a64348866f8dd4a9b32e2
Darshan Vinod Shetter <46949062+vsshetter@users.noreply.github.com>
2022-02-11 17:24:47 +0530

    Update conversion.h

--- 3_Implementation/inc/conversion.h
+++ 3_Implementation/inc/conversion.h
@@ -1,1 +1,16 @@
-dard
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include<math.h>
+
+
+void decitobina();
+void decitoocta();
+void decitohexa();
+void decitohexa();
+void bitodeci();
+void octtodeci();
+void hextodeci();
+
+
